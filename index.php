e492a88742bb2d54c46ad32289b41041d91001ab
Mark Popa <popamarknicolas1@gmail.com>
2024-04-15 10:02:12 +0200

    commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OSRS Questing Guide</title>
+</head>
+<body>
+
+    <h1>Old School Runescape F2P Quest Guide</h1>
+
+    <nav class="navbar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="quests.php">Quests</a></li>
+            <li><a href="money.php">Money Making</a></li>
+       </ul>
+    </nav>
+
+    <main>
+        <h3>Welcome to the Free 2 Play OSRS Quest Guide!!!</h3>
+    </main>
+</body>
+</html>
