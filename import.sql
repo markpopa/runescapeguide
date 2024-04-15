8f2b09d49a6f8b67e8974a2d2133b8ce8472e3ad
Mark Popa <popamarknicolas1@gmail.com>
2024-04-15 11:46:02 +0200

    commit

---
+++ import.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS runescape;
+
+USE runescape;
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS quests (
+    quest_id INT AUTO_INCREMENT PRIMARY KEY,
+    quest_name VARCHAR(100) NOT NULL,
+    quest_description TEXT
+);
--- index.php
+++ index.php
@@ -12,19 +12,36 @@
 
     <nav class="navbar">
         <ul>
+            <li><img src="logo.png" alt=""></li>
             <li><a href="index.php">Home</a></li>
             <li><a href="quests.php">Quests</a></li>
             <li><a href="money.php">Money Making</a></li>
        </ul>
     </nav>
 
-    <main>
+    <main class="content">
         <h3>Welcome to the Free 2 Play OSRS Quest Guide!!!</h3>
         <p>
         Welcome to our F2P OSRS questing hub! If you're diving into the world of Gielinor without a membership, you're in the right place. 
         Our website is all about helping you navigate through the exciting quests available in the Free-to-Play version of Old School RuneScape.
         From simple tasks like helping out in the kitchen to taking on legendary beasts, we've got guides and tips to make your journey smooth and enjoyable. So, grab your gear and get ready for some epic adventures!
+        </p><br>
+        <img src="Imp.webp"><br>
+        <h3>Simple Guide</h3>
+        <p>
+        This website is designed with simplicity and minimalism in mind. 
+        I've stripped away the clutter to provide you with a straightforward and easy-to-navigate OSRS F2P quest guide. 
+        No unnecessary distractions, just the information you need to complete your quests efficiently.
+        </p><br>
+        <img src="delrith.webp"><br>
+        <h3>Effective F2P money making</h3>
+        <p>
+        A problem you might encounter during your journey is money, but I have you covered!
+        There are a lot of ways to make money as a F2P player but some require lots of time
+        and might not even be worth it in the end. That's why I am going to share one of the most reliable
+        and my most used method to make GP!
         </p>
+        <img src="lavadragon.webp">
     </main>
 </body>
 </html>
