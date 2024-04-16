ccda0e723c02e7e256cd1cf34ee2f890b49caf4b
Mark Popa <popamarknicolas1@gmail.com>
2024-04-16 10:39:57 +0200

    commit

--- quests.php
+++ quests.php
@@ -1,3 +1,28 @@
 <?php 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quests</title>
+</head>
+<body>
+
+    <h1>F2P Quest List</h1>
+
+    <nav class="navbar">
+        <ul>
+            <li><img src="logo.png" alt=""></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="login.php">Log In</a></li>
+            <li><a href="logout.php">Log Out</a></li>
+            <li><a href="quests.php">Quests</a></li>
+            <li><a href="money.php">Money Making</a></li>
+       </ul>
+    </nav>
+    
+</body>
+</html>
